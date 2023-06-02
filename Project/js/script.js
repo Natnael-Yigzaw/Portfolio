e1a7dbe8902d1127b1db491ddffeb7d68130e32c
Natnael_Yigzaw <127339716+Natnael-Yigzaw@users.noreply.github.com>
2023-06-02 15:58:52 +0100

    first commit

---
+++ Project/js/script.js
@@ -0,0 +1,8 @@
+/*=============================Additionally this java script is used as text switcher between the text's i listed below.====================*/
+
+var typed = new Typed(".typing", {
+     strings:["An IT Student.", "Learning in Wolkite University.", "A Front-End Developer."],
+     typedSpeed:100,
+     BackSpeed:100,
+     loop:true
+})
